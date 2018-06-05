76a096531ff026d2ebaf9bc3b2fe70b1f1121c62
Takeshi <iwatakeshi@gmail.com>
2018-06-05 11:51:00 -0600

    Update: Add bubblesort

---
+++ Section 3/BubbleSort.java
@@ -0,0 +1,40 @@
+/**
+ * BubbleSort
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = { 20, 35, -15, 7, 55, 1, -22 };
+        print(array);
+        sort(array);
+        print(array);
+
+    }
+
+    public static void sort(int[] array) {
+        int last = array.length - 1;
+        
+        while (last != 0) {
+            for (int i = 0; i < last; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                }
+            }
+            last--;
+        }
+    }
+
+    private static void swap(int[] array, int a, int b) {
+        if (a == b) return;
+
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    private static void print(int[] array) {
+        for (int item : array) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+}
